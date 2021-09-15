f3c6cc933a224cd106098555b5e8d1612c7f6ac2
rishadm-ai7 <rishadm.ai7@gmail.com>
2021-09-15 18:12:54 +0530

    average of marks printed from the file

--- hadoop/student_red.py
+++ hadoop/student_red.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Q1:Total marks Scored by each student?
+#
+# import sys
+# currentname=None
+# currentmark=0
+#
+# for line in sys.stdin:
+#     line=line.strip()
+#     name,mark = line.split("\t")
+#     mark = int(mark)
+#
+#     if currentname==name:
+#         currentmark+=mark
+#
+#     else:
+#         if currentname!=None :
+#             print("%s\t%s"%(currentname,currentmark))
+#         currentname = name
+#         currentmark = mark
+#
+# print("%s\t%s"%(currentname,currentmark))
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Q2:Average marks Scored by each student?
+
 import sys
 currentname=None
 currentmark=0
+count=1
 
 for line in sys.stdin:
     line=line.strip()
@@ -11,16 +37,17 @@ for line in sys.stdin:
 
     if currentname==name:
         currentmark+=mark
+        count+=1
 
     else:
         if currentname!=None :
+            currentmark/=count
             print("%s\t%s"%(currentname,currentmark))
         currentname = name
         currentmark = mark
+        count = 1
 
+currentmark/=count
 print("%s\t%s"%(currentname,currentmark))
-
-
-
 
 
