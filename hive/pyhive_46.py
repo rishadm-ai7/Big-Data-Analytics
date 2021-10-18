aca456a8acc33f08c29956bb6b95df00a118839f
rishadm-ai7 <rishadm.ai7@gmail.com>
2021-10-18 21:16:49 +0530

    user defined function added;

--- hive/pyhive_46.py
+++ hive/pyhive_46.py
@@ -1,4 +1,4 @@
 from pyhive import hive
 cursor=hive.connect(host='localhost',database='batch46').cursor()
-cursor.execute('select * from customer')
+cursor.execute('select * from cust')
 print(cursor.fetchall())
---
+++ hive/udf_hive.py
@@ -0,0 +1,10 @@
+import sys
+
+for x in sys.stdin:
+    x=x.strip()
+    if " " in x:
+        fname,lname=x.split(" ")
+    else :
+        fname=x
+    lname=lname.lower()
+    print(" ".join([fname,lname]))
